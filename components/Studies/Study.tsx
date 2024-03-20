88fb1351ada8c3dd70184527597f6499c789373d
ir3n <iren.borada@gmail.com>
2024-03-20 22:54:44 +0200

    Fixes studies according to actual content

--- components/Studies/Study.tsx
+++ components/Studies/Study.tsx
@@ -3,13 +3,19 @@ import { StudyItem } from ".";
 
 const Study = ({ date, title, text, image }: StudyItem) => {
   return (
-    <div className="px-4 lg:pl-52 w-[310px] md:w-[600px] lg:w-[1000px] xl:w-[1100px]">
-      <div className="relative">
-        <div className="opacity-5 w-fit mx-auto max-w-[150px] md:max-w-[200px] lg:max-w-[400px] xl:max-w-full absolute lg:relative md:left-0 right-0 m-auto">
-          <Image src={image} alt={title} width={545} height={454} />
+    <div className="px-4 lg:pl-52 w-[310px] md:w-[600px] lg:w-[1000px] xl:w-[1100px] h-full">
+      <div className="relative h-full">
+        <div className="opacity-5 absolute top-0 lg:top-[50%] md:left-0 right-0 m-auto lg:translate-y-[-50%] max-w-[150px] md:max-w-[200px] lg:max-w-full">
+          <Image
+            src={image}
+            alt={title}
+            width={400}
+            height={400}
+            style={{ maxHeight: "300px", width: "auto", margin: "0 auto" }}
+          />
         </div>
-        <div className="lg:absolute top-0 left-0 right-0 mx-auto lg:flex gap-14 justify-between items-center h-full">
-          <div className="mb-4 lg:mb-0">
+        <div className="mx-auto lg:flex gap-14 items-center h-full">
+          <div className="mb-4 lg:mb-0 lg:max-w-80">
             <div className="text mb-2">{date}</div>
             <h4 className="small-title">{title}</h4>
           </div>
