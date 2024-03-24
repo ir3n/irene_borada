61437fc61ba1412fa558c9f286e24ab983196c47
ir3n <iren.borada@gmail.com>
2024-03-24 18:56:09 +0200

    Animation on outro arrow

--- components/Outro/index.tsx
+++ components/Outro/index.tsx
@@ -1,10 +1,13 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
 import Container from "../Container";
 import RotatingFlower from "../RotatingFlower";
 import { ArrowUp } from "lucide-react";
 import { accentColor } from "@/constants";
 
 const Outro = () => {
+  const [hover, setHover] = useState(false);
   const goBackToTop = () => {
     if (window) {
       window.scrollTo(0, 0);
@@ -23,11 +26,18 @@ const Outro = () => {
             with me!
           </div>
           <div
+            onMouseEnter={() => setHover(true)}
+            onMouseLeave={() => setHover(false)}
             onClick={goBackToTop}
             className="md:mt-16 text flex items-center leading-[1.1] gap-x-1.5 cursor-pointer w-fit"
+            data-cursor="-hidden"
           >
             go back to the <em>start</em>
-            <span className="max-w-[40px] md:max-w-full mb-2">
+            <span
+              className={`max-w-[40px] md:max-w-full mb-2  ${
+                hover ? "lg:translate-y-[-0.5rem]" : "lg:translate-y-1"
+              } transition duration-500`}
+            >
               <ArrowUp color={accentColor} size={60} />
             </span>
           </div>
