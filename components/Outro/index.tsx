d640e7247642126c9ac4992bf0109d6b2986b934
ir3n <iren.borada@gmail.com>
2024-04-06 23:32:21 +0300

    Fixes the outro link position

--- components/Outro/index.tsx
+++ components/Outro/index.tsx
@@ -35,23 +35,23 @@ const Outro = () => {
               </SlideFromBelow>
               <SlideFromBelow order={2}>with me!</SlideFromBelow>
             </div>
-            <div
-              onMouseEnter={() => setHover(true)}
-              onMouseLeave={() => setHover(false)}
-              onClick={goBackToTop}
-              className="md:mt-16 text flex items-center leading-[1.1] gap-x-1.5 cursor-pointer w-fit"
-              data-cursor="-hidden"
-            >
-              go back to the <em>start</em>
-              <span
-                className={`max-w-[40px] md:max-w-full mb-2  ${
-                  hover ? "lg:translate-y-[-0.5rem]" : "lg:translate-y-1"
-                } transition duration-500`}
-              >
-                <ArrowUp color={accentColor} size={60} />
-              </span>
-            </div>
           </Link>
+          <div
+            onMouseEnter={() => setHover(true)}
+            onMouseLeave={() => setHover(false)}
+            onClick={goBackToTop}
+            className="md:mt-16 text flex items-center leading-[1.1] gap-x-1.5 cursor-pointer w-fit"
+            data-cursor="-hidden"
+          >
+            go back to the <em>start</em>
+            <span
+              className={`max-w-[40px] md:max-w-full mb-2  ${
+                hover ? "lg:translate-y-[-0.5rem]" : "lg:translate-y-1"
+              } transition duration-500`}
+            >
+              <ArrowUp color={accentColor} size={60} />
+            </span>
+          </div>
         </Container>
         <div className="note text-center md:text-right absolute left-0 md:left-auto right-0 md:right-4 lg:right-[50px] bottom-4 lg:bottom-[50px]">
           designed & developed by Irene Borada
