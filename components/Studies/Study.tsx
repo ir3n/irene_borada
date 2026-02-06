6adb2c58b0d10c1933541f864fbd3d36fe4419ce
ir3n <iren.borada@gmail.com>
2026-02-06 19:44:24 +0100

    fix: make the individual study a client component

--- components/Studies/Study.tsx
+++ components/Studies/Study.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useRef, useState, useEffect } from "react";
 import Image from "next/image";
 import { useIsVisible } from "@/hooks/useIsVisible";
--- components/Studies/index.tsx
+++ components/Studies/index.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { GradientTitle } from "../GradientTitle";
 import SlideFromBelow from "../SlideFromBelow";
 import Study from "./Study";
